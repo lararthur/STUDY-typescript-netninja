684b9421203c03c46b2d91ced73732f2063ae595
polvora-fosforo <contaparacodigo999@gmail.com>
2020-10-22 10:47:33 -0300

    LESSON 8 - Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,3 +1,17 @@
 "use strict";
-// LESSON 7 Better Workflow & tsconfig - https://www.youtube.com/watch?v=Y4IiQY9dNRA&list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI&index=7
-console.log('Testing new structure');
+// LESSON 8 - Function Basics - https://www.youtube.com/watch?v=jXoSaX_yFh4&list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI&index=8
+var greet;
+greet = function () {
+    console.log('hello, again!');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 20; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,21 @@
-// LESSON 7 Better Workflow & tsconfig - https://www.youtube.com/watch?v=Y4IiQY9dNRA&list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI&index=7
+// LESSON 8 - Function Basics - https://www.youtube.com/watch?v=jXoSaX_yFh4&list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI&index=8
 
-console.log('Testing new structure');
+let greet: Function;
+
+greet = () => {
+    console.log('hello, again!');
+};
+
+const add = (a: number, b: number, c: number | string = 20): void => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, '20');
+
+const minus = (a: number, b: number): number => {
+    return a - b;
+}
+
+let result = minus(10, 7);
+console.log(result);
